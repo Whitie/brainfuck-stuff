2babca101e3011eb8e662852e5f4b69dc85c87f6
Whitie <weimann.th@gmail.com>
2020-12-06 09:17:44 +0100

    Added delayed execution.

--- bfg.py
+++ bfg.py
@@ -45,8 +45,10 @@ class BFInterpreterGUI(QtWidgets.QMainWindow):
         self.btn_stop.clicked.connect(self._stop)
         self.btn_run.clicked.connect(self.run)
         self.btn_step.clicked.connect(self.step)
+        self.delay_ms.valueChanged.connect(self._set_delay)
         self.tape.itemChanged.connect(self._tape_edited)
         self.tape.itemDoubleClicked.connect(self._user_change)
+        self.timer = None
         self.code = ''
         self.commands = {
             '+': (self.bf_increment, 'INCREMENT'),
@@ -65,9 +67,14 @@ class BFInterpreterGUI(QtWidgets.QMainWindow):
         self.tape_changed_by_user = False
         self._old_value = None
         self.stop = False
+        self.delay = 0
 
     def _show(self, message, timeout=5000):
         self.status.showMessage(message, timeout)
+
+    def _set_delay(self, value):
+        self.delay = value
+        print(value)
 
     def _stop(self):
         self.stop = True
@@ -169,6 +176,7 @@ class BFInterpreterGUI(QtWidgets.QMainWindow):
     def update_gui(self):
         self.program.selectColumn(self.pc)
         self.tape.selectColumn(self.ptr)
+        self.progress.setValue(self.pc)
         self.tape.item(0, self.ptr).setText(str(self.data[self.ptr]))
         self.tape.item(1, self.ptr).setText(chr(self.data[self.ptr]))
         self.tape.resizeColumnToContents(self.ptr)
@@ -181,10 +189,18 @@ class BFInterpreterGUI(QtWidgets.QMainWindow):
         except IndexError:
             text = 'PROGRAM END'
         self.journal.appendPlainText(text)
+        if self.timer is not None:
+            if self.timer.isActive() and self.timer.interval() != self.delay:
+                self.timer.setInterval(self.delay)
 
     def run(self):
-        while self.pc < len(self.code):
-            self.step()
+        if self.delay:
+            self.timer = QtCore.QTimer(self)
+            self.timer.timeout.connect(self.step)
+            self.timer.start(self.delay)
+        else:
+            while self.pc < len(self.code) and not self.stop:
+                self.step()
 
     def _check_tape(self):
         if self.ptr >= len(self.data):
@@ -196,6 +212,9 @@ class BFInterpreterGUI(QtWidgets.QMainWindow):
             command = self.code[self.pc]
         except IndexError:
             self._show('Programm beendet')
+            if self.timer is not None:
+                self.timer.stop()
+                self.timer = None
             return
         self.commands[command][0]()
         self.pc += 1
